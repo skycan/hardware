247b3ce30287e209583a5c0ee8b7ef65f1ec1c4c
Andres Vahter <andres.vahter@gmail.com>
2013-09-15 12:57:35 +0300

    Now BOM generator groups values

--- kicad-utils/bom-generator.py
+++ kicad-utils/bom-generator.py
@@ -21,6 +21,12 @@ projectpath = os.path.abspath(projectpath)
 for f in os.listdir(projectpath):
     inputfile = os.path.join(projectpath, f)
     
+    # hitting BOM button generates XML file and also zero byte file called "skycan-cdhs"
+    # lets remove this zero byte file here
+    if len(f.split(".")) == 1 and not os.path.isdir(inputfile):
+        os.remove(inputfile)
+        print 'deleted %s' % inputfile
+
     if f.split(".")[-1] == 'xml':
         print 'XML file found: %s' % inputfile
         netlistxml = inputfile
@@ -43,15 +49,65 @@ f = open(outputfilename, 'w')
 # create a new csv writer object to use as the output formatter
 out = csv.writer(f, lineterminator='\n', delimiter=',', quotechar="\"", quoting=csv.QUOTE_ALL)
 
-# output a field delimited header line
+components = net.getInterestingComponents()
+compfields = net.gatherComponentFieldUnion(components)
+partfields = net.gatherLibPartFieldUnion()
+
+# remove Reference, Value, Datasheet, and Footprint, they will come from 'columns' below
+partfields -= set( ['Reference', 'Value', 'Datasheet', 'Footprint'] )
+
+columnset = compfields | partfields     # union
+
+# prepend an initial 'hard coded' list and put the enchillada into list 'columns'
+columns = ['Reference', 'Value', 'LibPart', 'Footprint', 'Qty']
+defaultcolumncount = len(columns)
+columns = columns + sorted(list(columnset))
+
+# Create a new csv writer object to use as the output formatter
+out = csv.writer(f, lineterminator='\n', delimiter=',', quotechar='\"', quoting=csv.QUOTE_MINIMAL)
+
+# Output a set of rows as a header providing general information
 out.writerow(['Source:', net.getSource()])
 out.writerow(['Date:', net.getDate()])
 out.writerow(['Tool:', net.getTool()])
-out.writerow(['Component Count:', len(net.components)])
-out.writerow(['Ref', 'Value', 'Footprint', 'Farnell'])
+out.writerow(['Component Count:', len(components)])
+out.writerow([])
+out.writerow(['Components:'])
+out.writerow([])
+out.writerow(columns)
 
-components = net.getInterestingComponents()
+# Get all of the components in groups of matching parts + values (see kicad_netlist_reader.py)
+grouped = net.groupComponents(components)
 
-# output all of the component information
-for c in components:
-    out.writerow([c.getRef(), c.getValue(), c.getFootprint(), c.getField("Farnell")])
+# Output component information organized by group, aka as collated:
+row = []
+for group in grouped:
+    del row[:]
+    refs = ""
+
+    # Add the reference of every component in the group and keep a reference
+    # to the component so that the other data can be filled in once per group
+    for component in group:
+        if len(refs) > 0:
+            refs += ", "
+        refs += component.getRef()
+        c = component
+
+    # Fill in the component groups common data
+    row.append(refs);
+    row.append(c.getValue())
+    row.append(c.getLibName() + ":" + c.getPartName())
+    row.append(net.getGroupFootprint(group))
+    row.append(len(group))
+
+    # from column xx upwards, use the fieldnames to grab the data
+    for field in columns[defaultcolumncount:]:
+        row.append( net.getGroupField(group, field) );
+
+    out.writerow( row )
+
+# we do not need XML file anymore, lets delete it
+print '\nCSV file generated, delete input XML file %s' % netlistxml
+os.remove(netlistxml)
+
+f.close()
